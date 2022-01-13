8de6b49f009fd37d6e704693065e7853ade8837d
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-01-13 13:04:32 +0100

    Add module.css file for component styling

--- todos/src/components/TodoItem.js
+++ todos/src/components/TodoItem.js
@@ -1,18 +1,22 @@
 import React from 'react';
-// import TodoContainer from './TodoContainer';
+import styles from '../styles/TodoItem.module.css'
 
 class TodoItem extends React.PureComponent {
   render() {
     return (
-      <div>
-      <input
-        type="checkbox"
-        checked={this.props.todo.completed}
-        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-/>
-<button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
-</div>
-    );
+      <li className={styles.item}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        {this.props.todo.title}
+      </li>
+    )
   }
 }
 
