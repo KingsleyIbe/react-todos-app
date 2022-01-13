4cea3eb76e37a871f5a5124baa24d788e698fdc1
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-01-13 17:47:19 +0100

    Refactor code to use Hooks on a different directory

--- todos/src/index.js
+++ todos/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoContainer from './components/TodoContainer';
+// import TodoContainer from './classBased/components/TodoContainer';
+import TodoContainer from './functionBased/components/TodoContainer';
 import './styles/App.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <TodoContainer /> */}
     <TodoContainer />
   </React.StrictMode>,
   document.getElementById('root'),
